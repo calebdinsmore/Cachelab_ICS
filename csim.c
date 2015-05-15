4c20fb14e5fd1451c9527bc78a716c3e4f711ead
cdinsmor <cdinsmor@matthew.cse.taylor.edu>
2015-05-15 00:56:19 -0400

    Newest commit

--- csim.c
+++ csim.c
@@ -4,6 +4,21 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+
+int parseTrace(char operation, char *addr, int size, int verbose, int set, int assoc, int blockBits){
+	int S = pow(2,set);
+	int B = pow(2,blockBits);
+	int *arr[S];
+
+	for ( int i = 0; i < S; i++ ) {
+		arr[i] = (int *) malloc((2 + (B)) * assoc);
+	}
+
+	arr[0][3] = 4;
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,13 +27,12 @@ int main(int argc, char *argv[])
 	int s;
 	int E;
 	int b;
-	char *t[1];
+	char *t[10];
 	FILE *traceFile;
 	
 	s = 0;
 	E = 0;
 	b = 0;
-	*t[0] = 'c';
 	s = s + E + b + v;
 	while ( (c = getopt(argc, argv, ":hvs:E:b:t:")) != -1 ) {
 		//printf("%c\n", c);
@@ -47,18 +61,29 @@ int main(int argc, char *argv[])
 
 	char value[20] = "aaaaaaaaaaaaaaaaaaaa"; 
 	int idx = 0;
-	//char operation = ' ';
-	//int addr = 0;
-	//int size = 0;
+	int counter = 0;
+	char operation = ' ';
+	char *addr;
+	int size = 0;
 	while ( (nextCh = fgetc(traceFile))  != EOF ) {
-		printf("%c", nextCh);
-		//while ( !strchr(" ,\n", nextCh) ) {
-			//value[idx++] = nextCh;
-		//	nextCh = fgetc(traceFile);
-		//}
-		//value[idx] = '\0';
+		//printf("%c", nextCh);
+		while ( !strchr(" ,\n", nextCh) ) {
+			value[idx++] = nextCh;
+			nextCh = fgetc(traceFile);
+		}
+		value[idx] = '\0';
 		idx = 0;
-		//printf("%s\n", value);
+		if ( strcmp("",value) ) {
+			if ( counter == 0 ) operation = value[0];
+			else if ( counter == 1 ) sscanf(value, "%s", addr);
+			else { 
+				sscanf(value, "%d", &size);
+				counter = -1;
+				parseTrace(operation, addr, size, v, s , E, b);
+			}
+
+			counter++;
+		}
 	}
     printSummary(0, 0, 0);
     return 0;
